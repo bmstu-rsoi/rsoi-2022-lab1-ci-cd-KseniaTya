f1879c6809754d9517beb63e0c4fde800b68b382
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-25 23:26:14 +0300

    fix urls (2)

--- persons/urls.py
+++ persons/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/api/v1/persons/', views.persons, name='persons'),
-    path('/api/v1/persons/<person_id>', views.person, name='person'),
-    path('/api/v1/create_person/', views.create_person, name='create_person'),
-    path('/api/v1/update_person/<person_id>', views.update_person, name='update_person'),
-    path('/api/v1/delete_person/<person_id>', views.delete_person, name='delete_person'),
+    path('api/v1/persons/', views.persons, name='persons'),
+    path('api/v1/persons/<person_id>', views.person, name='person'),
+    path('api/v1/create_person/', views.create_person, name='create_person'),
+    path('api/v1/update_person/<person_id>', views.update_person, name='update_person'),
+    path('api/v1/delete_person/<person_id>', views.delete_person, name='delete_person'),
 ]
